e391507830c525c4b3e1498772a3a583a1004990
NITHINPOLI04 <145465193+NITHINPOLI04@users.noreply.github.com>
2025-05-20 12:14:28 +0530

    Create 3024-Type of Triangle.py

---
+++ LeetCode/Easy/3024-Type of Triangle.py
@@ -0,0 +1,18 @@
+#LINk: https:https://leetcode.com/problems/type-of-triangle/
+
+# LeetCode 3024. Single Number
+# Time: O(1), Space: O(1)
+
+class Solution:
+    def triangleType(self, nums: List[int]) -> str:
+        a, b, c = sorted(nums)
+
+        if a + b <= c:
+            return 'none'
+
+        if a == b == c:
+            return 'equilateral'
+        elif a == b or b == c:
+            return 'isosceles'
+        else:
+            return 'scalene'
