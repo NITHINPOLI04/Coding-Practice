9017031946eb1b456320572fe42a9efb3d695d06
NITHINPOLI04 <145465193+NITHINPOLI04@users.noreply.github.com>
2025-05-23 22:59:05 +0530

    Create 274-H-Index.py

---
+++ LeetCode/Medium/274-H-Index.py
@@ -0,0 +1,27 @@
+#LINk: https://leetcode.com/problems/single-number/
+
+# LeetCode 274. H-Index
+# Approach 1: Using Extra Variable h
+# Time: O(n), Space: O(1)
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        h = 0
+        citations.sort(reverse = True)
+        for i in range(len(citations)):
+            if citations[i] >= i + 1:
+                h = i + 1
+            else:
+                break
+        return h
+
+------------------------------------------------------
+
+# Approach 2: Without using any Extra Variable 
+# Time: O(nlogn), Space: O(1)
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort(reverse = True)
+        for i in range(len(citations)):
+            if citations[i] < i + 1:
+                return i
+        return len(citations)
