b58c633fdc11ce7b83b7bf27bc9888879269a44f
NITHINPOLI04 <145465193+NITHINPOLI04@users.noreply.github.com>
2025-07-23 15:52:36 +0530

    Create 1695-Maximum Erasure Value.py

---
+++ LeetCode/Medium/1695-Maximum Erasure Value.py
@@ -0,0 +1,23 @@
+#LINk: https://leetcode.com/problems/maximum-erasure-value/
+
+# LeetCode 1695. Maximum Erasure Value
+# Time: O(n), Space: O(n)
+
+class Solution:
+    def maximumUniqueSubarray(self, nums: List[int]) -> int:
+        seen = set()
+        left = 0
+        curr_sum = 0
+        max_sum = 0
+
+        for right in range(len(nums)):
+            while nums[right] in seen:
+                seen.remove(nums[left])
+                curr_sum -= nums[left]
+                left += 1
+
+            seen.add(nums[right])
+            curr_sum += nums[right]
+            max_sum = max(max_sum, curr_sum)
+
+        return max_sum
