5eef9d6c4e790bfcc038c19191b4fddbc7c3e4c9
NITHINPOLI04 <145465193+NITHINPOLI04@users.noreply.github.com>
2025-08-11 18:49:03 +0530

    Create 1093-Uncle Johny.py

---
+++ CodeChef/1093-Uncle Johny.py
@@ -0,0 +1,19 @@
+#Difficulty: 1093
+# Time: O(1), Space: O(1)
+
+def solve():
+    n = int(input())
+    a = list(map(int, input().split()))
+    k = int(input())
+    
+    target = a[k-1]
+    sorted_a = sorted(a)
+    
+    for i in range(n):
+        if sorted_a[i] == target:
+            print(i+1)
+            return
+
+t = int(input())
+for _ in range(t):
+    solve()
